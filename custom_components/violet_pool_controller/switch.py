296cbcdf89ea27ae97685647e4ef161a0ce659ae
Basti <89860334+Xerolux@users.noreply.github.com>
2024-09-28 21:04:30 +0200

    Update switch.py

--- custom_components/violet_pool_controller/switch.py
+++ custom_components/violet_pool_controller/switch.py
@@ -2,7 +2,21 @@ import logging
 import aiohttp
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from .const import DOMAIN, API_SET_FUNCTION_MANUALLY
+import async_timeout
+
+from .const import (
+    DOMAIN, 
+    CONF_API_URL, 
+    CONF_POLLING_INTERVAL, 
+    CONF_USE_SSL, 
+    CONF_DEVICE_ID,
+    CONF_USERNAME, 
+    CONF_PASSWORD,
+    DEFAULT_POLLING_INTERVAL, 
+    DEFAULT_USE_SSL,
+    API_READINGS,
+    API_SET_FUNCTION_MANUALLY
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,6 +30,12 @@ class VioletSwitch(CoordinatorEntity, SwitchEntity):
         self.ip_address = coordinator.ip_address
         self.username = coordinator.username
         self.password = coordinator.password
+        self.use_ssl = coordinator.use_ssl
+        self.session = coordinator.session
+        if not all([self.ip_address, self.username, self.password]):
+            _LOGGER.error("Missing credentials or IP address for the VioletSwitch")
+        else:
+            _LOGGER.info(f"VioletSwitch initialized for {self._key} with IP {self.ip_address}")
 
     def _get_switch_state(self):
         return self.coordinator.data.get(self._key)
@@ -24,47 +44,31 @@ class VioletSwitch(CoordinatorEntity, SwitchEntity):
     def is_on(self):
         return self._get_switch_state() == 1
 
-    async def async_turn_on(self, **kwargs):
+    async def _send_command(self, action):
+        protocol = "https" if self.use_ssl else "http"
+        url = f"{protocol}://{self.username}:{self.password}@{self.ip_address}{API_SET_FUNCTION_MANUALLY}?{self._key},{action},0,0"
         try:
-            if self.username and self.password and self.ip_address:
-                url = f"http://{self.username}:{self.password}@{self.ip_address}{API_SET_FUNCTION_MANUALLY}?{self._key},ON,0,0"
-                async with self.coordinator.session.post(url) as response:
+            async with async_timeout.timeout(10):  # Setting a 10-second timeout
+                async with self.session.post(url) as response:
                     response.raise_for_status()
-                _LOGGER.debug(f"Successfully turned on {self._key} at {url}")
-            else:
-                _LOGGER.error(f"Missing credentials or IP address for {self._key}")
+                    _LOGGER.debug(f"Successfully sent {action} command to {self._key} at {url}")
         except aiohttp.ClientError as err:
-            _LOGGER.error(f"Error turning on {self._key}: {err}")
+            _LOGGER.error(f"Client error sending {action} command to {self._key}: {err}")
+        except aiohttp.ClientResponseError as resp_err:
+            _LOGGER.error(f"Response error sending {action} command to {self._key}: {resp_err.status} {resp_err.message}")
+        except asyncio.TimeoutError:
+            _LOGGER.error(f"Timeout sending {action} command to {self._key}")
         except Exception as err:
-            _LOGGER.error(f"Unexpected error when turning on {self._key}: {err}")
+            _LOGGER.error(f"Unexpected error when sending {action} command to {self._key}: {err}")
+
+    async def async_turn_on(self, **kwargs):
+        await self._send_command("ON")
 
     async def async_turn_off(self, **kwargs):
-        try:
-            if self.username and self.password and self.ip_address:
-                url = f"http://{self.username}:{self.password}@{self.ip_address}{API_SET_FUNCTION_MANUALLY}?{self._key},OFF,0,0"
-                async with self.coordinator.session.post(url) as response:
-                    response.raise_for_status()
-                _LOGGER.debug(f"Successfully turned off {self._key} at {url}")
-            else:
-                _LOGGER.error(f"Missing credentials or IP address for {self._key}")
-        except aiohttp.ClientError as err:
-            _LOGGER.error(f"Error turning off {self._key}: {err}")
-        except Exception as err:
-            _LOGGER.error(f"Unexpected error when turning off {self._key}: {err}")
+        await self._send_command("OFF")
 
     async def async_turn_auto(self, **kwargs):
-        try:
-            if self.username and self.password and self.ip_address:
-                url = f"http://{self.username}:{self.password}@{self.ip_address}{API_SET_FUNCTION_MANUALLY}?{self._key},AUTO,0,0"
-                async with self.coordinator.session.post(url) as response:
-                    response.raise_for_status()
-                _LOGGER.debug(f"Successfully set {self._key} to auto at {url}")
-            else:
-                _LOGGER.error(f"Missing credentials or IP address for {self._key}")
-        except aiohttp.ClientError as err:
-            _LOGGER.error(f"Error setting {self._key} to auto: {err}")
-        except Exception as err:
-            _LOGGER.error(f"Unexpected error when setting {self._key} to auto: {err}")
+        await self._send_command("AUTO")
 
     @property
     def icon(self):
@@ -74,9 +78,7 @@ class VioletSwitch(CoordinatorEntity, SwitchEntity):
             return "mdi:lightbulb-on" if self.is_on else "mdi:lightbulb"
         elif self._key == "ECO":
             return "mdi:leaf" if self.is_on else "mdi:leaf-off"
-        elif self._key == "DOS_1_CL":
-            return "mdi:flask" if self.is_on else "mdi:atom"
-        elif self._key == "DOS_4_PHM":
+        elif self._key == "DOS_1_CL" or self._key == "DOS_4_PHM":
             return "mdi:flask" if self.is_on else "mdi:atom"
         return self._icon
 
