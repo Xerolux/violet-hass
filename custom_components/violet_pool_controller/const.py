0051a3d2c1d8618742df67e7e87162d9fa7b2e54
Basti <89860334+Xerolux@users.noreply.github.com>
2025-03-26 06:55:58 +0100

    Update const.py

--- custom_components/violet_pool_controller/const.py
+++ custom_components/violet_pool_controller/const.py
@@ -26,7 +26,7 @@ DEFAULT_POOL_TYPE = "outdoor"
 DEFAULT_DISINFECTION_METHOD = "chlorine"
 
 # Integrationsdetails
-INTEGRATION_VERSION = "0.1.0"  # Erhöhte Version für die überarbeitete Integration
+INTEGRATION_VERSION = "0.0.9.5"  # Erhöhte Version für die überarbeitete Integration
 
 # Logger-Name
 LOGGER_NAME = f"{DOMAIN}_logger"  # Einheitlich mit den anderen Dateien
